b752296cfe736dd235b046423595ff799d4c1fc3
Jackson Bond <111647134+linwayhuang@users.noreply.github.com>
2025-03-09 22:08:17 -0500

    made the 0 button span 2 column

--- javascript.js
+++ javascript.js
@@ -7,13 +7,17 @@ const buttonArray = [
     {text: '7'}, {text: '8'}, {text: '9'}, {text: '-'},
     {text: '4'}, {text: '5'}, {text: '6'}, {text: '*'},
     {text: '1'}, {text: '2'}, {text: '3'}, {text: '/'},
-    {text: '0'}, {text: '00'}, {text: '.'}, {text: '='},
+    {text: '0', classes: 'wide'}, {text: '.'}, {text: '='},
 ];
 
 buttonArray.forEach(item => {
     const btn = document.createElement("button");
     btn.classList.add("button-style");
     btn.textContent = item.text;
+    /*If we specified extra classes*/
+    if (item.classes) {
+        item.classes.split(" ").forEach(cls => btn.classList.add(cls));
+    }
     buttonContainer.appendChild(btn);
 })
 
